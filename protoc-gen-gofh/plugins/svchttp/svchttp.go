041a070e67141c8fc24e9668b6c7cce4d2872f40
Simon Menke <simon.menke@gmail.com>
2016-01-11 17:00:45 +0100

    feat(cli): Added better cli command

--- protoc-gen-gofh/plugins/svchttp/svchttp.go
+++ protoc-gen-gofh/plugins/svchttp/svchttp.go
@@ -1,11 +1,12 @@
 package svchttp
 
 import (
-	"fmt"
-	"os"
+	"strconv"
 	"strings"
 
+	"github.com/fd/featherhead/pkg/api/httpapi/router"
 	"github.com/fd/featherhead/proto"
+	"github.com/gogo/protobuf/gogoproto"
 	"github.com/gogo/protobuf/proto"
 	pb "github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
@@ -17,6 +18,10 @@ const (
 	contextPkgPath   = "golang.org/x/net/context"
 	grpcPkgPath      = "google.golang.org/grpc"
 	grpcCodesPkgPath = "google.golang.org/grpc/codes"
+	httpPkgPath      = "net/http"
+	routerPkgPath    = "github.com/fd/featherhead/pkg/api/httpapi/router"
+	errorsPkgPath    = "github.com/juju/errors"
+	jsonPkgPath      = "encoding/json"
 )
 
 func init() {
@@ -68,6 +73,10 @@ func (g *svchttp) Generate(file *generator.FileDescriptor) {
 	g.P("var _ context_svchttp.Context")
 	g.P("var _ grpc_svchttp.Codec")
 	g.P("var _ codes_svchttp.Code")
+	g.P(`var _ *http_svchttp.Request`)
+	g.P(`var _ *router_svchttp.Router`)
+	g.P(`var _ = errors_svchttp.Errorf`)
+	g.P(`var _ = json_svchttp.Marshal`)
 	g.P()
 	for i, service := range file.FileDescriptorProto.Service {
 		g.generateService(file, service, i)
@@ -82,6 +91,10 @@ func (g *svchttp) GenerateImports(file *generator.FileDescriptor) {
 	g.gen.PrintImport("context_svchttp", contextPkgPath)
 	g.gen.PrintImport("grpc_svchttp", grpcPkgPath)
 	g.gen.PrintImport("codes_svchttp", grpcCodesPkgPath)
+	g.gen.PrintImport("http_svchttp", httpPkgPath)
+	g.gen.PrintImport("router_svchttp", routerPkgPath)
+	g.gen.PrintImport("errors_svchttp", errorsPkgPath)
+	g.gen.PrintImport("json_svchttp", jsonPkgPath)
 }
 
 func unexport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
@@ -89,12 +102,16 @@ func unexport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
 // generateService generates all the code for the named service.
 func (g *svchttp) generateService(file *generator.FileDescriptor, service *pb.ServiceDescriptorProto, index int) {
 
-	// origServName := service.GetName()
-	// servName := generator.CamelCase(origServName)
+	origServName := service.GetName()
+	servName := generator.CamelCase(origServName)
 
-	// Server interface.
-	// innerServerType := servName + "Server"
-	// serverType := unexport(servName) + "ServerGateway"
+	innerServerType := servName + "Server"
+	handlerName := unexport(servName) + "Handler"
+
+	g.P("type ", handlerName, " struct {")
+	g.P("ss ", innerServerType)
+	g.P("}")
+	g.P()
 
 	// Server handler implementations.
 	for _, method := range service.Method {
@@ -110,37 +127,140 @@ func (g *svchttp) generateService(file *generator.FileDescriptor, service *pb.Se
 		outputTypeName := method.GetOutputType()
 		outputType, _ := g.gen.ObjectNamed(outputTypeName).(*generator.Descriptor)
 
+		var (
+			pattern    string
+			httpMethod string
+		)
+
+		switch x := info.GetPattern().(type) {
+		case *fhannotations.HttpRule_Get:
+			httpMethod = "GET"
+			pattern = x.Get
+		case *fhannotations.HttpRule_Post:
+			httpMethod = "POST"
+			pattern = x.Post
+		case *fhannotations.HttpRule_Put:
+			httpMethod = "PUT"
+			pattern = x.Put
+		case *fhannotations.HttpRule_Patch:
+			httpMethod = "PATCH"
+			pattern = x.Patch
+		case *fhannotations.HttpRule_Delete:
+			httpMethod = "DELETE"
+			pattern = x.Delete
+		default:
+			g.gen.Fail("xyz.featherhead.http requires a method: pattern")
+		}
+
+		vars, err := router.ExtractVariables(pattern)
+		if err != nil {
+			g.gen.Error(err)
+			return
+		}
+
 		//g.gen.GetFieldName(message *generator.Descriptor, field *descriptor.FieldDescriptorProto)
 
-		fmt.Fprintf(os.Stderr, "info: %v\n", info)
-		fmt.Fprintf(os.Stderr, "  input:  %v\n", inputType)
-		fmt.Fprintf(os.Stderr, "  output: %v\n", outputType)
+		handlerMethod := g.generateServerCallName(servName, method)
+		g.P("func (h* ", handlerName, " )", handlerMethod, "(ctx context_svchttp.Context, rw http_svchttp.ResponseWriter, req *http_svchttp.Request) error {")
+		g.P("// info:      ", info.String())
+		g.P("//   method:  ", httpMethod)
+		g.P("//   pattern: ", pattern)
+		g.P("//   input:   ", inputType.String())
+		g.P("//   output:  ", outputType.String())
+		g.P("if req.Method != ", strconv.Quote(httpMethod), "{")
+		g.P(`  return errors_svchttp.MethodNotAllowedf("expected `, httpMethod, ` request")`)
+		g.P("}")
+		g.P()
+
+		g.P(`var (`)
+		g.P(`input `, g.typeName(inputTypeName))
+		if len(vars) > 0 {
+			g.P(`params = router_svchttp.P(ctx)`)
+		}
+		g.P(`)`)
+		g.P()
+
+		if httpMethod == "POST" || httpMethod == "PUT" || httpMethod == "PATCH" {
+			g.P("{ // from body")
+			g.P("err := json_svchttp.NewDecoder(req.Body).Decode(&input)")
+			g.P(`if err != nil {`)
+			g.P(`return errors_svchttp.Trace(err)`)
+			g.P(`}`)
+			g.P(`}`)
+			g.P()
+		}
+
+		for _, v := range vars {
+			g.P("{ // populate ", v.Name)
+			g.P("var msg0 = &input")
+
+			partType := inputType
+			parts := strings.Split(v.Name, ".")
+			lastIdx := len(parts) - 1
+			for i, part := range parts {
+				field := partType.GetFieldDescriptor(part)
+				fieldGoName := g.gen.GetFieldName(partType, field)
+
+				if i == lastIdx {
+					partType = nil
+					if !field.IsString() {
+						g.gen.Fail("expected a string")
+					}
+
+					g.P("msg", i, ".", fieldGoName, " = params.Get(", strconv.Quote(v.Name), ")")
+
+				} else {
+					if !field.IsMessage() {
+						g.gen.Fail("expected a message")
+					}
+
+					if gogoproto.IsNullable(field) {
+						g.P(`if msg`, i, `.`, fieldGoName, ` == nil {`)
+						g.P("msg", i, ".", fieldGoName, " = &", g.typeName(field.GetTypeName()), "{}")
+						g.P(`}`)
+						g.P("var msg", i+1, " = msg", i, ".", fieldGoName)
+					} else {
+						g.P("var msg", i+1, " = &msg", i, ".", fieldGoName)
+					}
+
+					partType = g.gen.ObjectNamed(field.GetTypeName()).(*generator.Descriptor)
+				}
+			}
+
+			g.P("}")
+			g.P()
+		}
+
+		g.P(`{ // call`)
+		if !method.GetServerStreaming() && !method.GetClientStreaming() {
+			g.P(`output, err := h.ss.`, g.generateServerCall(servName, method))
+		} else {
+			g.P(`err := h.ss.`, g.generateServerCall(servName, method))
+		}
+		g.P(`if err != nil {`)
+		g.P(`return errors_svchttp.Trace(err)`)
+		g.P(`}`)
+		g.P(`_ = output`)
+		g.P(`}`)
+		g.P()
+
+		g.P("return nil")
+		g.P("}")
+		g.P()
+
 	}
 
 }
 
 // generateServerSignature returns the server-side signature for a method.
-func (g *svchttp) generateServerSignature(servName string, method *pb.MethodDescriptorProto) string {
+func (g *svchttp) generateServerCallName(servName string, method *pb.MethodDescriptorProto) string {
 	origMethName := method.GetName()
-	methName := generator.CamelCase(origMethName)
+	methName := "_http_" + servName + "_" + generator.CamelCase(origMethName)
 	if reservedClientName[methName] {
 		methName += "_"
 	}
 
-	var reqArgs []string
-	ret := "(err error)"
-	if !method.GetServerStreaming() && !method.GetClientStreaming() {
-		reqArgs = append(reqArgs, "ctx context_svchttp.Context")
-		ret = "(out *" + g.typeName(method.GetOutputType()) + ", err error)"
-	}
-	if !method.GetClientStreaming() {
-		reqArgs = append(reqArgs, "input *"+g.typeName(method.GetInputType()))
-	}
-	if method.GetServerStreaming() || method.GetClientStreaming() {
-		reqArgs = append(reqArgs, "stream "+servName+"_"+generator.CamelCase(origMethName)+"Server")
-	}
-
-	return methName + "(" + strings.Join(reqArgs, ", ") + ") " + ret
+	return methName
 }
 
 // generateServerSignature returns the server-side signature for a method.
@@ -156,7 +276,7 @@ func (g *svchttp) generateServerCall(servName string, method *pb.MethodDescripto
 		reqArgs = append(reqArgs, "ctx")
 	}
 	if !method.GetClientStreaming() {
-		reqArgs = append(reqArgs, "input")
+		reqArgs = append(reqArgs, "&input")
 	}
 	if method.GetServerStreaming() || method.GetClientStreaming() {
 		reqArgs = append(reqArgs, "stream")
